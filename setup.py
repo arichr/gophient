34b120ad35159b5a9c9b1fd5af5aa94df60e3fb8
Arisu W <87308853+arichr@users.noreply.github.com>
2021-07-14 12:35:02 +0300

    Gophient 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gophient',
-    version='0.2',
+    version='0.3',
     url='https://github.com/arichr/gophient',
     license='MIT',
     author='Arisu W.',
